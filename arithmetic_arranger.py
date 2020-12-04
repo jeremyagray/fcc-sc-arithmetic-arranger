4b0bd95169faad7694b642901f46bd3c2e59af1d
Jeremy A Gray <jeremy.a.gray@gmail.com>
2020-12-04 09:50:39 -0600

    perf: streamline several operations
    Streamline several operations and dropped requirement for re module.
    
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,14 +1,12 @@
-import re
-
-
 def arithmetic_arranger(problems, answers=False):
     if len(problems) > 5:
         return "Error: Too many problems."
 
-    pline_one = ''
-    pline_two = ''
-    pline_bars = ''
-    pline_ans = ''
+    line_one = ''
+    line_two = ''
+    line_bars = ''
+    line_ans = ''
+
     for index, problem in enumerate(problems):
         # Split the problem.
         [one, op, two] = problem.split()
@@ -18,39 +16,35 @@ def arithmetic_arranger(problems, answers=False):
             return "Error: Operator must be '+' or '-'."
 
         # Only digits.
-        if (re.search(r"\D", one) or re.search(r"\D", two)):
+        if not (one.isdigit() and two.isdigit()):
             return "Error: Numbers must only contain digits."
 
         # Four digits only.
         if (len(one) > 4 or len(two) > 4):
             return "Error: Numbers cannot be more than four digits."
 
-        one_length = len(one)
-        two_length = len(two)
-        if one_length > two_length:
-            length = one_length
-        else:
-            length = two_length
+        length = max(len(one), len(two))
 
-        pline_one += '  ' + str(one).rjust(length, ' ')
-        pline_two += op + ' ' + str(two).rjust(length, ' ')
-        pline_bars += '-'.rjust(length + 2, '-')
         if (op == '+'):
             ans = int(one) + int(two)
-        else:
+        elif (op == '-'):
             ans = int(one) - int(two)
-        pline_ans += str(ans).rjust(length + 2, ' ')
+
+        line_one += '  ' + str(one).rjust(length, ' ')
+        line_two += op + ' ' + str(two).rjust(length, ' ')
+        line_bars += '-'.rjust(length + 2, '-')
+        line_ans += str(ans).rjust(length + 2, ' ')
 
         if ((index + 1) < len(problems)):
-            pline_one += '    '
-            pline_two += '    '
-            pline_bars += '    '
-            pline_ans += '    '
+            line_one += '    '
+            line_two += '    '
+            line_bars += '    '
+            line_ans += '    '
 
     if answers:
-        arranged_problems = (pline_one + '\n' + pline_two
-                             + '\n' + pline_bars + '\n' + pline_ans)
+        arranged_problems = (line_one + '\n' + line_two
+                             + '\n' + line_bars + '\n' + line_ans)
     else:
-        arranged_problems = pline_one + '\n' + pline_two + '\n' + pline_bars
+        arranged_problems = line_one + '\n' + line_two + '\n' + line_bars
 
     return arranged_problems
