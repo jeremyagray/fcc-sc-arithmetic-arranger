9c81c5e3e6b2e578c9117bfa0ef55fba46f014ca
Jeremy A Gray <jeremy.a.gray@gmail.com>
2024-05-25 12:42:30 -0500

    fix(tooling): remove obsolete tooling
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

--- .dir-locals.el
+++
@@ -1,3 +0,0 @@
-((python-mode
-  (flycheck-python-flake8-executable . "/home/gray/.virtualenvs/fcc-sc-arithmetic-arranger/bin/flake8")
-  (flycheck-python-pylint-executable . "/home/gray/.virtualenvs/fcc-sc-arithmetic-arranger/bin/flake8")))
